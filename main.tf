277c2967df30320ad1c37cb71b48d6ef804e1bf0
saidevops7722 <saiaws7722@gmail.com>
2024-02-19 21:30:45 +0530

    terraform infra

--- main.tf
+++ main.tf
@@ -26,11 +26,17 @@ resource "aws_subnet" "public_subnets" {
 resource "aws_route_table" "public-route-table" {
   vpc_id = aws_vpc.main.id
 
-  for_each          = var.public_subnets
-  tags       = merge(
+  for_each = var.public_subnets
+  tags     = merge(
     var.tags,
     { Name = "${var.env}-${each.value["name"]}" }
   )
+}
+resource "aws_route_table_association" "public-association" {
+  for_each       = var.public_subnets
+  subnet_id      = lookup(lookup(aws_subnet.public_subnets, each.value["name"], null), "id", null)
+  //subnet_id      = aws_subnet.public_subnets[each.value["name"]].id
+  route_table_id = aws_route_table.public-route-table[each.value["name"]].id
 }
 
 ## private subnets
