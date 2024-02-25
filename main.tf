eecf330b233427e2a55e9f7f1102377da2b1633f
saidevops7722 <saiaws7722@gmail.com>
2024-02-25 11:21:05 +0530

    terraform infra

--- main.tf
+++ main.tf
@@ -13,6 +13,10 @@ resource "aws_vpc_peering_connection" "peer" {
   peer_vpc_id   = var.default_vpc_id
   vpc_id        = aws_vpc.main.id
   auto_accept   = true
+  tags       = merge(
+    var.tags,
+    { Name = "${var.env}-peer" }
+  )
 }
 
 ## public subnets
@@ -70,6 +74,11 @@ resource "aws_route_table" "public-route-table" {
     gateway_id = aws_internet_gateway.igw.id
   }
 
+  route {
+    cidr_block = data.aws_vpc.default_vpc.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  }
+
   for_each = var.public_subnets
   tags     = merge(
     var.tags,
@@ -107,6 +116,12 @@ resource "aws_route_table" "private-route-table" {
     cidr_block = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat-gateways["public-${split("-", each.value["name"])[1]}"].id
   }
+
+  route {
+    cidr_block = data.aws_vpc.default_vpc.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  }
+
   tags       = merge(
     var.tags,
     { Name = "${var.env}-${each.value["name"]}" }
@@ -119,4 +134,11 @@ resource "aws_route_table_association" "private-association" {
   route_table_id = aws_route_table.private-route-table[each.value["name"]].id
 }
 
+## route to the default vpc for peering to work
+
+resource "aws_route" "route" {
+  route_table_id         = var.default_route_table
+  destination_cidr_block = var.vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
 
